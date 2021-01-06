0048b8dd381628b82ad2d3491ff8d5294195273d
20192137 <d.p.meulendijks@student.tue.nl>
2021-01-06 19:32:15 +0100

    Put taking measurements also in scheduler

--- main.py
+++ main.py
@@ -2,26 +2,31 @@
 
 import hardwarespi
 import takepicsendpic
+import logcurrentreadings
 from apscheduler.schedulers.blocking import BlockingScheduler
 import datetime
-import logcurrentreadings
 
 
 def take_image_and_send():
-    namecurrtime = datetime.datetime.now()
-    takepicsendpic.takepic(namecurrtime, "./pics")
-    takepicsendpic.sendpic(namecurrtime, "./pics")
+    namecurrtimepic = datetime.datetime.now()           # get current time to save with timestamp
+    takepicsendpic.takepic(namecurrtimepic, "./pics")   # take the picture and save it
+    takepicsendpic.sendpic(namecurrtimepic, "./pics")   # take the saved picture and send it to the database
+
+def take_measurements_and_send():
+    namecurrtimemeasure = datetime.datetime.now()       # get current time to save with timestamp
+    data = [hardwarespi.read_spi(i) for i in range(8)]  # read pin on MCP3008
+    logcurrentreadings.tologs(data)                     # save the readings to a log file
 
 
 
 if __name__ == '__main__':
-    scheduler = BlockingScheduler()
-    scheduler.add_job(take_image_and_send, 'interval', hours=1)
-    scheduler.start()
     hardwarespi.setup_spi()
-    while True:
-        try:
-            data = [hardwarespi.read_spi(i) for i in range(8)] #maybe if this is wrong set it to range(1,9) # read pin on MCP3008
-            logcurrentreadings.tologs(data)
-        finally:
-            pass
+
+    # Calls the function take_image_and_send every hour
+    picscheduler = BlockingScheduler()
+    picscheduler.add_job(take_image_and_send, 'interval', hours=1)
+    picscheduler.start()
+    # Calls the function take_measurements_and_send every 30 minutes
+    measscheduler = BlockingScheduler()
+    measscheduler.add_job(take_measurements_and_send, 'interval', minutes=30)
+    measscheduler.start()
