3c917874bcae98f91377013a575dbe20fea3dba8
20192137 <d.p.meulendijks@student.tue.nl>
2021-01-11 12:47:47 +0100

    data to integers

--- logcurrentreadings.py
+++ logcurrentreadings.py
@@ -11,19 +11,20 @@ def toLogs(data):
 
 def sendLog(data_received, id = '249824748730293804'):
     print(f'sending log data {data_received}')
+    data_received_int = [int(i) for i in data_received]
     mp_encoder = MultipartEncoder(
         fields={
             'id': id,
             # plain file object, no filename or mime type produces a
             # Content-Disposition header with just the part name
-            'temperature': data_received[0],
-            'brightness1': data_received[1],
-            'brightness2': data_received[2],
-            'brightness3': data_received[3],
-            'brightness4': data_received[4],
-            'moistureair': data_received[5],
-            'moistureground': data_received[6],
-            'score': data_received[7]
+            'temperature': data_received_int[0],
+            'brightness1': data_received_int[1],
+            'brightness2': data_received_int[2],
+            'brightness3': data_received_int[3],
+            'brightness4': data_received_int[4],
+            'moistureair': data_received_int[5],
+            'moistureground': data_received_int[6],
+            'score': data_received_int[7]
         }
     )
     r = requests.post(
